2cfe701d962a2a79401f18417c22d13e48a66002
Priyankha1011 <111988064+Priyankha1011@users.noreply.github.com>
2022-12-07 16:50:01 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -90,39 +90,7 @@
             Submit
              </v-btn>
         </v-card-text>
-        <!-- <v-divider class="mt-12"></v-divider>
-        <v-card-actions>
-          <v-btn text>
-            Cancel
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-slide-x-reverse-transition>
-            <v-tooltip
-              v-if="formHasErrors"
-              left
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  icon
-                  class="my-0"
-                  v-bind="attrs"
-                  @click="resetForm"
-                  v-on="on"
-                >
-                  <v-icon>mdi-refresh</v-icon>
-                </v-btn>
-              </template>
-              <span>Refresh form</span>
-            </v-tooltip>
-          </v-slide-x-reverse-transition>
-          <v-btn
-            color="primary"
-            text
-            @click="submit"
-          >
-            Submit
-          </v-btn>
-        </v-card-actions> -->
+      
       </v-card>
     </v-col>
   </v-row>
@@ -164,7 +132,7 @@ import {addDatas } from "../service"
         console.log("data added", datas);
 
       await addDatas.addData(datas);
-      // alert("Complaint Registered Successfully !")
+      alert("Complaint Registered Successfully !")
     },
   }
    
